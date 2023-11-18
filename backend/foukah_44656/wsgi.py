72b738946c7c23075a82c301db1cd32c1b4e8ae3
Crowdbotics <team@crowdbotics.com>
2023-11-18 03:53:06 +0000

    Initial commit

---
+++ backend/foukah_44656/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for foukah_44656 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "foukah_44656.settings"
+)
+
+application = get_wsgi_application()
